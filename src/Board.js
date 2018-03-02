94ea3862c20352895082295e52ea3db3a769b0af
Livia_Santos <liviamelq@gmail.com>
2018-03-03 10:38:58 +1100

    Change Board syntax to native classes

--- src/Board.js
+++ src/Board.js
@@ -1,49 +1,47 @@
-import React from 'react'
+import React, { Component } from 'react'
 import './index.css'
 import Note from "./Note"
 
-var Board = React.createClass({
-  propTypes: {
-    count: function(props, propName) {
-      if(typeof props[propName] !== "number") {
-        return new Error("The count must be a number")
-      }
-      if(props[propName] > 100) {
-        return new Error('Creating ' + props[propName]
-          + ' notes is ridiculous')
-      }
+class Board extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+        notes: []
     }
-  },
-
-  getInitialState(){
-    return {
-      notes: []
+  }
+  componentWillMount() {
+    var self = this
+    if(this.props.count) {
+      fetch(`https://baconipsum.com/api/?type=all-meat&sentences=${this.props.count}`)
+        .then(response => response.json())
+        .then(json => json[0]
+                          .split(". ")
+                          .forEach(sentence => self.add(sentence.substring(0, 24))))
     }
-  },
+  }
 
-  componentWillMount(){
-    if (this.props.count) {
-      var url = `https://baconipsum.com/api/?type=all-meat&sentences=${this.props.count}`
-      fetch(url)
-        .then(results => results.json())
-        .then(array => array[0])
-        .then(text => text.split('. '))
-        .then(array => array.forEach(
-            sentence => this.add(sentence)
-        ))
-        .catch(function(err) {
-          console.log("Didn't connect to the API", err)
-        })
-    }
+  // componentWillMount = () => {
+  //   if (this.props.count) {
+  //     var url = `https://baconipsum.com/api/?type=all-meat&sentences=${this.props.count}`
+  //     fetch(url)
+  //       .then(results => results.json())
+  //       .then(array => array[0])
+  //       .then(text => text.split('. '))
+  //       .then(array => array.forEach(
+  //           sentence => this.add(sentence)
+  //       ))
+  //       .catch(function(err) {
+  //         console.log("Didn't connect to the API", err)
+  //       })
+  //   }
+  // }
 
-  },
-
-  nextId(){
+  nextId = () => {
     this.uniqueId = this.uniqueId || 0
     return this.uniqueId++
-  },
+  }
 
-  add(text) {
+  add = (text) => {
     var notes = [
       ...this.state.notes,
       {
@@ -52,9 +50,9 @@ var Board = React.createClass({
       }
     ]
     this.setState({notes})
-  },
+  }
 
-  update(newText, id) {
+  update = (newText, id) => {
     var notes = this.state.notes.map(
       note => (note.id !== id) ?
         note :
@@ -64,21 +62,21 @@ var Board = React.createClass({
         }
       )
     this.setState({notes})
-  },
+  }
 
-  remove(id){
+  remove = (id) => {
     var notes = this.state.notes.filter(note => note.id !== id)
     this.setState({notes})
-  },
+  }
 
-  eachNote(note){
+  eachNote = (note) => {
     return(<Note key={note.id}
                   id={note.id}
                   onChange={this.update}
                   onRemove={this.remove}>
             {note.note}
           </Note>)
-  },
+  }
 
   render() {
     return(<div className='board'>
@@ -86,7 +84,7 @@ var Board = React.createClass({
             <button onClick={()=> this.add("New Note")}>+</button>
           </div>)
   }
-})
+}
 
 
 export default Board;
